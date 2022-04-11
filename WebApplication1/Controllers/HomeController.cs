cedf1aa2f3469f7b4cf1f99c035c9aab0636ced4
蔡壮茂 <70785748@qq.com>
2022-04-11 17:05:37 +0800

    no message

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string db = ConfigurationManager.ConnectionStrings["db"].ConnectionString;
+            string appKey = ConfigurationManager.AppSettings["appKey"];
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
